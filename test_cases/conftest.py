c1305c27ad9cdebaa94f8963036dd3cfcb08c6ae
Rishikrsh Thorve <rthorve29@gmail.com>
2026-05-12 15:58:19 +0530

    changes for using login as a background

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,34 +1,3 @@
-# import pytest
-# #from selenium.webdriver.chrome.options import Options
-# import undetected_chromedriver as uc
-# from selenium import webdriver
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-
-
-# options = uc.ChromeOptions()
-
-# @pytest.fixture()
-# def set_up(browser):
-#     if browser == "chrome":
-#         driver = uc.Chrome()
-
-#     elif browser == "firefox":
-#         driver = webdriver.Firefox()
-#     else:
-#         driver = uc.Chrome()
-#     driver.maximize_window()
-#     driver.implicitly_wait(10)  # Add implicit wait
-
-    
-#     return driver
-
-# def pytest_addoption(parser):
-#     parser.addoption("--browser")
-
-# @pytest.fixture()
-# def browser(request):
-#     return request.config.getoption("--browser")
 
 import pytest
 import undetected_chromedriver as uc
