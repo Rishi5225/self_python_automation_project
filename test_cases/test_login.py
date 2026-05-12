e21eff28886c5f108858faa5071e8a005122e86f
Rishikrsh Thorve <rthorve29@gmail.com>
2026-05-12 15:53:22 +0530

    Corrected the login setup as background

--- test_cases/test_login.py
+++ test_cases/test_login.py
@@ -1,47 +1,69 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from utilities.read_properties import ReadConfigProperties
-from page_objects.login import Login
-import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from utilities.read_properties import ReadConfigProperties
+# from page_objects.login import Login
+# import time
 
 
-class TestLoginPage:
-    base_url = ReadConfigProperties.get_url()
-    username = ReadConfigProperties.get_username()
-    password = ReadConfigProperties.get_password()
+# class TestLoginPage:
+#     base_url = ReadConfigProperties.get_url()
+#     username = ReadConfigProperties.get_username()
+#     password = ReadConfigProperties.get_password()
 
-    def test_login_page_title(self,set_up):
-        self.driver = set_up
-        self.driver.get(self.base_url)
-        time.sleep(10)
+#     def test_login_page_title(self,set_up):
+#         self.driver = set_up
+#         self.driver.get(self.base_url)
+#         time.sleep(10)
 
-        page_title = self.driver.title
+#         page_title = self.driver.title
 
-        if page_title == "OrangeHRM":
-            self.driver.close()
-            assert True
+#         if page_title == "OrangeHRM":
+#             self.driver.close()
+#             assert True
         
-        else:
-            self.driver.save_screenshot(r"F:\\self_python_automation_project\\screen_shots\\failed.png")
-            assert False
+#         else:
+#             self.driver.save_screenshot(r"F:\\self_python_automation_project\\screen_shots\\failed.png")
+#             assert False
 
-    def test_login(self,set_up):
-        self.driver = set_up
-        self.driver.get(self.base_url)
+#     def test_login(self,set_up):
+#         self.driver = set_up
+#         self.driver.get(self.base_url)
 
-        self.login_pg_obj = Login(self.driver)
-        self.login_pg_obj.set_username(self.username)
-        self.login_pg_obj.set_password(self.password)
-        self.login_pg_obj.click_login()
-        time.sleep(10)
+#         self.login_pg_obj = Login(self.driver)
+#         self.login_pg_obj.set_username(self.username)
+#         self.login_pg_obj.set_password(self.password)
+#         self.login_pg_obj.click_login()
+#         time.sleep(10)
 
-        page_title = self.driver.title
+#         page_title = self.driver.title
 
-        if page_title == "OrangeHRM":
-            self.driver.close()
-            assert True
+#         if page_title == "OrangeHRM":
+#             self.driver.close()
+#             assert True
 
-        else:
-            self.driver.save_screenshot(r"F:\\self_python_automation_project\\screen_shots\\failed.png")
-            self.driver.close()
-            assert False
+#         else:
+#             self.driver.save_screenshot(r"F:\\self_python_automation_project\\screen_shots\\failed.png")
+#             self.driver.close()
+#             assert False
+
+from selenium.webdriver.common.by import By
+
+
+class TestDashboard:
+
+    def test_dashboard_title(self, login_setup):
+
+        self.driver = login_setup
+
+        assert self.driver.title == "OrangeHRM"
+
+    def test_dashboard_header(self, login_setup):
+
+        self.driver = login_setup
+
+        dashboard = self.driver.find_element(
+            By.XPATH,
+            "//h6[text()='Dashboard']"
+        ).text
+
+        assert dashboard == "Dashboard"
