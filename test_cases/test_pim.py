d197b23383fd1a2f37697f2d2274a96485de5a6e
Rishikrsh Thorve <rthorve29@gmail.com>
2026-05-24 19:42:58 +0530

    new-conftest-test-case

--- test_cases/test_pim.py
+++ test_cases/test_pim.py
@@ -26,3 +26,47 @@ class TestPIM:
         pim.click_save()
 
         assert pim.validate_personal_details_header()
+
+
+class TestPIMConfiguration:
+
+    def test_configuration_dropdown_validation(self, login_setup):
+
+        self.driver = login_setup
+
+        pim = PIMPage(self.driver)
+
+        pim.click_pim_menu()
+
+        pim.click_configuration_menu()
+
+        assert pim.validate_optional_fields()
+
+        assert pim.validate_custom_fields()
+
+        assert pim.validate_data_import()
+
+        assert pim.validate_reporting_methods()
+
+        assert pim.validate_termination_reasons()
+
+
+class TestPIMOptionalFields:
+
+    def test_optional_fields_toggle_validation(self, login_setup):
+
+        self.driver = login_setup
+
+        pim = PIMPage(self.driver)
+
+        pim.click_pim_menu()
+
+        pim.click_configuration_menu()
+
+        pim.click_optional_fields()
+
+        pim.turn_on_all_toggles()
+
+        pim.turn_off_all_toggles()
+
+        pim.click_save_button()
