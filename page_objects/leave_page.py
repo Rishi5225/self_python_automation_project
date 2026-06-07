b88804651edfb3a79d812db89f243ba43c75fb66
Rishi5225 <rthorve29@gmail.com>
2026-06-07 17:51:50 +0530

    made the page and test files of test more cicd aligned

--- page_objects/leave_page.py
+++ page_objects/leave_page.py
@@ -2,62 +2,61 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 class LeavePage:
 
     # ---------------- LEFT MENU ----------------
 
-    leave_menu_xpath = "//span[text()='Leave']"
+    leave_menu_xpath = "//span[normalize-space()='Leave']"
 
     # ---------------- TOP TABS ----------------
 
-    apply_tab_xpath = "//a[text()='Apply']"
+    apply_tab_xpath = "//a[normalize-space()='Apply']"
 
-    my_leave_tab_xpath = "//a[text()='My Leave']"
+    my_leave_tab_xpath = "//a[normalize-space()='My Leave']"
 
-    entitlements_tab_xpath = "//span[text()='Entitlements ']"
+    entitlements_tab_xpath = "//span[normalize-space()='Entitlements']"
 
-    reports_tab_xpath = "//span[text()='Reports ']"
+    reports_tab_xpath = "//span[normalize-space()='Reports']"
 
-    configure_tab_xpath = "//span[text()='Configure ']"
+    configure_tab_xpath = "//span[normalize-space()='Configure']"
 
-    leave_list_tab_xpath = "//a[text()='Leave List']"
+    leave_list_tab_xpath = "//a[normalize-space()='Leave List']"
 
-    assign_leave_tab_xpath = "//a[text()='Assign Leave']"
+    assign_leave_tab_xpath = "//a[normalize-space()='Assign Leave']"
 
     # ---------------- ENTITLEMENTS DROPDOWN ----------------
 
-    add_entitlements_xpath = "//a[text()='Add Entitlements']"
+    add_entitlements_xpath = "//a[normalize-space()='Add Entitlements']"
 
-    employee_entitlements_xpath = "//a[text()='Employee Entitlements']"
+    employee_entitlements_xpath = "//a[normalize-space()='Employee Entitlements']"
 
-    my_entitlements_xpath = "//a[text()='My Entitlements']"
+    my_entitlements_xpath = "//a[normalize-space()='My Entitlements']"
 
     # ---------------- REPORTS DROPDOWN ----------------
 
-    leave_entitlements_report_xpath = "//a[text()='Leave Entitlements and Usage Report']"
+    leave_entitlements_report_xpath = "//a[contains(.,'Leave Entitlements and Usage Report')]"
 
-    my_leave_entitlements_report_xpath = "//a[text()='My Leave Entitlements and Usage Report']"
+    my_leave_entitlements_report_xpath = "//a[contains(.,'My Leave Entitlements and Usage Report')]"
 
     # ---------------- CONFIGURE DROPDOWN ----------------
 
-    leave_period_xpath = "//a[text()='Leave Period']"
+    leave_period_xpath = "//a[normalize-space()='Leave Period']"
 
-    leave_types_xpath = "//a[text()='Leave Types']"
+    leave_types_xpath = "//a[normalize-space()='Leave Types']"
 
-    work_week_xpath = "//a[text()='Work Week']"
+    work_week_xpath = "//a[normalize-space()='Work Week']"
 
-    holidays_xpath = "//a[text()='Holidays']"
+    holidays_xpath = "//a[normalize-space()='Holidays']"
 
     # ---------------- PAGE HEADERS ----------------
 
-    apply_leave_header_xpath = "//h6[text()='Apply Leave']"
+    apply_leave_header_xpath = "//h6[normalize-space()='Apply Leave']"
 
-    my_leave_header_xpath = "//h5[text()='My Leave List']"
+    my_leave_header_xpath = "//h5[normalize-space()='My Leave List']"
 
-    leave_list_header_xpath = "//h5[text()='Leave List']"
+    leave_list_header_xpath = "//h5[normalize-space()='Leave List']"
 
-    assign_leave_header_xpath = "//h6[text()='Assign Leave']"
+    assign_leave_header_xpath = "//h6[normalize-space()='Assign Leave']"
 
     # ---------------- CONSTRUCTOR ----------------
 
@@ -69,18 +68,35 @@ class LeavePage:
 
     def click_element(self, xpath):
 
-        WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(
+        element = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(
                 (By.XPATH, xpath)
             )
-        ).click()
+        )
+
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView({block:'center'});",
+            element
+        )
+
+        WebDriverWait(self.driver, 20).until(
+            EC.visibility_of(element)
+        )
+
+        self.driver.execute_script(
+            "arguments[0].click();",
+            element
+        )
 
     # ---------------- GENERIC VALIDATION ----------------
 
     def validate_element_visible(self, xpath):
 
-        return WebDriverWait(self.driver, 10).until(
+        element = WebDriverWait(self.driver, 20).until(
             EC.visibility_of_element_located(
                 (By.XPATH, xpath)
             )
-        ).is_displayed()
+        )
+
+        return element.is_displayed()
+
